976998a1187d7ae889c5e16812b23b1b4d621c79
daguitosama <tocarralero@gmail.com>
2023-01-17 15:37:16 -0500

    baseline

---
+++ README.md
@@ -0,0 +1,9 @@
+# AR 101
+
+A project to learn about AR
+
+## Task #1
+Render a sheet with an object name near that object
+
+### Task #1 Notes
+-   my current GPS coords 23.058814679991926, -82.39097650695497
---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>AR 1010</h1>
+        <p>Pieces:</p>
+        <ul>
+            <li>
+                <a href="/mac-tag"> Mac Tag</a>
+            </li>
+        </ul>
+    </div>
+</template>
