aba171ceb4217d1c95a1c9cde7bf49527c7e606c
daguitosama <tocarralero@gmail.com>
2023-01-18 00:05:20 -0500

    feat: play first selector

--- components/NewGameForm.vue
+++ components/NewGameForm.vue
@@ -1,11 +1,21 @@
 <script setup>
-//
+function startGame() {
+    console.log("starting game");
+}
 </script>
 
 <template>
-    <div>
+    <div class="grid gap-[30px]">
         <PlayerAvatarSelector />
-        <!-- <PlayerFirstSelector /> -->
-        <!-- <NewGameBtn /> -->
+        <PlayerFirstSelector />
+        <div
+            class="px-[30px] bg-white fixed w-full bottom-8 left-0 flex items-center justify-center"
+        >
+            <PrimaryBtn
+                @click="startGame"
+                text="Play"
+            />
+            <!-- <p>foo</p> -->
+        </div>
     </div>
 </template>
--- components/PlayerFirstSelector.vue
+++ components/PlayerFirstSelector.vue
@@ -1,7 +1,18 @@
 <script setup>
-const { playerGoFirst } = usePlayerGoFirst();
+const { playerGoFirst } = useGameData();
 </script>
 
 <template>
-    <div></div>
+    <button
+        @click="playerGoFirst = !playerGoFirst"
+        class="flex gap-4 items-center justify-center border-black border-2 p-[10px] rounded-xl transition-all duration-300 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-black focus-visible:ring-offset-4"
+    >
+        <span class="text-xl font-medium">Play First</span>
+        <div
+            class="w-[16px] h-[16px] rounded-full border-2 border-black bg-white transition-all duration-300"
+            :class="{
+                'bg-black ': playerGoFirst,
+            }"
+        ></div>
+    </button>
 </template>
